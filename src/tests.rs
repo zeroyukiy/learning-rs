f9992580359b8a6473a02dfaa1a22dea2a09dc49
Yuri Montobbio <zeroyukiy@gmail.com>
2019-08-24 22:23:17 +0200

    Tests: assert!, assert_eq! assert_ne!, failure messages, should_panic, Result<T, E>

---
+++ src/tests.rs
@@ -0,0 +1,81 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello {}", name)
+}
+
+struct Guess {
+    value: i32
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err("two plus two does not equal to four".to_string())
+        }
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let bigger = Rectangle { width: 20, height: 90 };
+        let smaller = Rectangle { width: 18,height: 77 };
+        
+        assert!(bigger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn it_not_adds_two() {
+        assert_ne!(4, add_two(3));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
